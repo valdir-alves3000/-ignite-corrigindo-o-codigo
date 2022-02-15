a9a5d8dc258e7d7b6cde393cbb494dcfde744080
valdir-alves3000 <51173956+valdir-alves3000@users.noreply.github.com>
2022-02-15 14:02:47 -0300

    Challenge completed successfully

---
+++ REDME.md
@@ -0,0 +1,21 @@
+# Corrigindo o Código>
+
+ <p align="center">
+      <img alt="Desafio Ignite" title="Desafio Ignite" src="src/assets/desafio-ignite.jpg" />
+</p>
+
+Este é um projeto desenvolvido durante o ignite, como forma de desafio para dissiminação de conhecimento dentro da [Rocketseat](https://app.rocketseat.com.br/ignite), venha fazer parte deste time! :wink:
+
+## Composição do nosso projeto
+
+Neste projeto Temos alguns **Endpoints Base** que podem ser extendidos da forma mais adequada para seu contexto.
+
+São eles:
+
+### Usuários
+
+- GET /repositories
+- POST /repositories
+- PUT /repositories/:id
+- DELETE /repositories/:id
+- POST /repositories/:id/like
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { v4: uuid } = require("uuid");
+const { v4: uuid, validate } = require("uuid");
 
 const app = express();
 
@@ -13,28 +13,46 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs } = request.body
+  const { title, url, techs } = request.body;
 
   const repository = {
     id: uuid(),
     title,
     url,
     techs,
-    likes: 0
+    likes: 0,
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const verifyId = validate(id);
+
+  if (!verifyId) {
+    return response.status(404).json({
+      error: "Invalid ID!",
+    });
+  }
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
+
+  const updatedRepository = {
+    title: title ?? repositories[repositoryIndex].title,
+    url: url ?? repositories[repositoryIndex].url,
+    techs: techs ?? repositories[repositoryIndex].techs,
+  };
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
@@ -46,9 +64,19 @@ app.put("/repositories/:id", (request, response) => {
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const verifyId = validate(id);
 
-  if (repositoryIndex > 0) {
+  if (!verifyId) {
+    return response.status(404).json({
+      error: "Invalid ID!",
+    });
+  }
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -60,7 +88,9 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
@@ -68,7 +98,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json({ likes });
 });
 
 module.exports = app;
